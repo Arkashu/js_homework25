2c13a66b725d28c1fd36285d18a6eca03164714f
Arkashu <kopeikaarkashu@gmail.com>
2022-08-07 17:09:58 +0300

    new methods

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict'
 const arr1 = [2, 5, 6, 0, 6, 3, 7];
 
+// --------------------------indexOf-------------------------------
 const indexOf = (arr = [], searchElement, fromIndex = 0) => {
     for (let i = fromIndex; i < arr.length; i++) {
         if (arr[i] === searchElement){
@@ -8,6 +9,8 @@ const indexOf = (arr = [], searchElement, fromIndex = 0) => {
         }
     } return -1;
 }
+
+// --------------------------lastIndexOf-------------------------------
 
 const lastIndexOf = (arr = [], searchElement, fromIndex = arr.length - 1) => {
     for (let i = fromIndex; i >= 0; i--) {
@@ -17,11 +20,61 @@ const lastIndexOf = (arr = [], searchElement, fromIndex = arr.length - 1) => {
     } return -1;
 }
 
+// --------------------------find-------------------------------
+
 const find = (arr = [], searchElement) => {
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === searchElement){
+        if (searchElement(arr[i], i, arr)){
             return arr[i];
         }
-    } return undefined;
+    }
 }
+find(arr1, (item, index, arr) => {
+    return item === 4;
+})
 
+//-----------------------------findIndex--------------------------
+const findIndex = (arr, callback) =>{
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i], i, arr)) {
+            return i;
+        }
+    } return -1;
+}
+findIndex(arr1, (item, index, arr) => {
+    return item === 6;
+});
+
+// ------------------------------every-----------------------------
+const every = (arr, callback) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (!callback(arr[i], i, arr)){
+            return false;
+        }
+    } return true;
+}
+every(arr1, (item, index, arr) => {
+    return item === 6;
+});
+
+//-------------------------------includes----------------------------
+const includes = (arr, searchElement, fromIndex = 0) => {
+    for (let i = fromIndex; i < arr.length; i++) {
+        if (arr[i] === searchElement){
+            return true;
+        }
+    } return false;
+}
+includes(arr1, 4);
+
+//--------------------------------some--------------------------------
+const some = (arr, callback) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i], i, arr)){
+            return true;
+        }
+    } return false;
+}
+some(arr1, (item, index, arr) => {
+    return item === 6;
+});
